b7bbf1d238c5c0de450b145e620cb36c2a8cd3c3
Tom <tomtomdinh@gmail.com>
2018-06-02 01:13:34 -0700

    add queries playground file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -72,6 +72,7 @@ describe('GET /todos', ()=> {
       .expect(200)
       .expect((res) => {
         expect(res.body.todos.length).toBe(2);
+        // figure how to iterate through array and check if text matches
       })
       .end(done);
   });
