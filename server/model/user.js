914409788514085803fa15e0d7e02d515c0e4144
Tom <tomtomdinh@gmail.com>
2018-06-04 20:26:35 -0700

    add test for POST users/login

--- server/model/user.js
+++ server/model/user.js
@@ -91,10 +91,12 @@ UserSchema.statics.findByCredentials = function(email, password) {
 
     return new Promise((resolve,reject) => {
       bcrypt.compare(password, user.password, (err, res) => {
-        if(!res) {
+        if(res) {
+          resolve(user);
+        } else {
           reject();
         }
-        resolve(user);
+        
       });
     });
   });
