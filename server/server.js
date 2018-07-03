47cbe2e3fdf2e2ca32c1c525d120469a32230b86
Tom <tomtomdinh@gmail.com>
2018-07-02 22:17:55 -0700

    converted some promises to async-await

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
-// cmd command to start server mongod.exe --dbpath /Users/Tom/mongo-data
+
+// change directory to C:\Program Files\MongoDB\Server\3.6\bin
+// cmd command to start database server mongod.exe --dbpath /Users/Tom/mongo-data
+
+// manages local environments
 require('./config/config.js');
 
 const express = require('express');
@@ -72,23 +76,39 @@ app.get('/todos/:id', authenticate, (req,res)=> {
   });
 });
 
-app.delete('/todos/:id', authenticate, (req,res) => {
-  var id = req.params.id;
+app.delete('/todos/:id', authenticate, async (req,res) => {
 
-  if(!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
+  try {
+    const id = req.params.id;
 
-  Todo.findOneAndRemove({
-    _id: id,
-    _creator: req.user._id
-  }).then((todo)=> {
+    if(!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+
+    const todo = await Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+    });
+
     if(!todo) {
       return res.status(404).send();
     }
 
     res.send({todo});
-  }).catch((e)=>res.status(400).send());
+  } catch (e) {
+    res.status(400).send();
+  }
+
+  // Todo.findOneAndRemove({
+  //   _id: id,
+  //   _creator: req.user._id
+  // }).then((todo)=> {
+  //   if(!todo) {
+  //     return res.status(404).send();
+  //   }
+  //
+  //   res.send({todo});
+  // }).catch((e)=>res.status(400).send());
 
 });
 
@@ -124,46 +144,64 @@ app.patch('/todos/:id', authenticate, (req,res) => {
 });
 
 // POST /users
-app.post('/users',(req,res)=> {
-  // gets the email and password from the post request
-  var body = _.pick(req.body, 'email', 'password');
-
-  // sets the email and password within this user
-  var user = new User(body);
-
-  user.save().then(() => {
-    return user.generateAuthToken();
-  })
-  .then((token)=> {
+app.post('/users',async (req,res)=> {
+  try {
+    // gets the email and password from the post request
+    const body = _.pick(req.body, 'email', 'password');
+    // sets the email and password within this user
+    const user = new User(body);
+    await user.save();
+    const token = await user.generateAuthToken();
     // putting x before an access creates a custom header
     res.header('x-auth', token).send(user);
-  })
-  .catch((e) => res.status(400).send(e));
+  } catch (e) {
+    res.status(400).send(e);
+  }
+//   user.save().then(() => {
+//     return user.generateAuthToken();
+//   })
+//   .then((token)=> {
+//
+//   })
+//   .catch((e) => res.status(400).send(e));
 });
 
 app.get('/users/me', authenticate, (req,res)=>{
   res.send(req.user);
 });
 
-app.post('/users/login', (req,res) => {
-  var body = _.pick(req.body, 'email', 'password');
-
-  User.findByCredentials(body.email, body.password).then((user) => {
-    user.generateAuthToken().then((token)=> {
-      res.header('x-auth', token).send(user);
-    });
-  }).catch((e) => {
+app.post('/users/login', async (req,res) => {
+  try {
+    const body = _.pick(req.body, 'email', 'password');
+    const user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (e) {
     res.status(400).send();
-  });
+  }
+
+  // User.findByCredentials(body.email, body.password).then((user) => {
+  //   return user.generateAuthToken().then((token)=> {
+  //     res.header('x-auth', token).send(user);
+  //   });
+  // }).catch((e) => {
+  //   res.status(400).send();
+  // });
 
 });
 
-app.delete('/users/me/token', authenticate, (req,res) => {
-  req.user.removeToken(req.token).then(()=> {
-      res.status(200).send();
-  }, ()=> {
+app.delete('/users/me/token', authenticate, async (req,res) => {
+  try {
+    await req.user.removeToken(req.token);
+    res.status(200).send();
+  } catch (e) {
     res.status(400).send();
-  });
+  }
+  // req.user.removeToken(req.token).then(()=> {
+  //     res.status(200).send();
+  // }, ()=> {
+  //   res.status(400).send();
+  // });
 });
 
 
